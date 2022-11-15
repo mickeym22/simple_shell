f0972f2a687979058ec183eb94fa408f525b8e4a
Amsale Tadiwos <111055724+Amsale-Tad@users.noreply.github.com>
2022-11-15 21:36:33 +0300

    Update string.c

--- string.c
+++ string.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns the length of a strings
  * @s: the string whose length to check
  *
  * Return: integer length of string
